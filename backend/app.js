3a0bebe0539e82244ba33c94bc7666e3ac26f2b6
VigneshVikki18 <vikydgl2000@gmail.com>
2025-05-05 21:44:18 +0530

    final

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://peaceful-sawine-8cfedf.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
